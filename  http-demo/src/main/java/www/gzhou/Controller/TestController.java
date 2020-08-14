5a103a0b9adb46891c94ee98ff8368ac204e681c
GG_0 <596847321@qq.com>
2020-08-14 22:55:15 +0800

    feat:测试

---
+++  http-demo/src/main/java/www/gzhou/Controller/TestController.java
@@ -0,0 +1,22 @@
+package www.gzhou.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @Author: TuMing.Chen
+ * CatKing
+ * @Date: 2020/8/14 22:37
+ */
+@Controller
+@RequestMapping("test")
+public class TestController {
+
+    @RequestMapping("/1")
+    @ResponseBody
+    public String TestPrint(){
+        return "Hello Wold";
+    }
+
+}
---
+++  http-demo/src/main/java/www/gzhou/StartSpringBootTest.java
@@ -0,0 +1,16 @@
+package www.gzhou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: TuMing.Chen
+ * CatKing
+ * @Date: 2020/8/14 22:33
+ */
+@SpringBootApplication
+public class StartSpringBootTest {
+    public static void main(String[] args) {
+        SpringApplication.run(StartSpringBootTest.class, args);
+    }
+}
